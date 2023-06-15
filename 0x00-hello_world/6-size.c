495da3d03967add2f059c7dd8ee4cba1cb4886fb
Mohamed Samir <79877083+mohamedsamir77@users.noreply.github.com>
2023-06-15 16:49:59 +0400

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int l_int_var;
 long long int ll_int_var;
 float f_int;
 
-printf("Size of a cahr: "+ sizeof(char_var)+ " byte(s)\n");
-printf("Size of an int: "+ sizeof(int_var)+ " byte(s)\n");
-printf("Size of a long int: "+ sizeof(l_int_var)+ " byte(s)\n");
-printf("Size of a long long int: "+ sizeof(ll_int_var)+ " byte(s)\n");
-printf("Size of a float: "+ sizeof(f_int)+ " byte(s)\n");
+printf("Size of a cahr:", sizeof(char_var), "byte(s)\n");
+printf("Size of an int:", sizeof(int_var), "byte(s)\n");
+printf("Size of a long int:", sizeof(l_int_var), "byte(s)\n");
+printf("Size of a long long int:", sizeof(ll_int_var), "byte(s)\n");
+printf("Size of a float:", sizeof(f_int), "byte(s)\n");
 return (0);
 }
