400a7e1c974fc475113ca6818df0a676d9696d19
mohamedsamir77 <geo_msamir77@yahoo.com>
2023-06-15 05:33:21 -0700

    adding all the files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /* start of the program */
+{
+puts("\"Programming is like building a multilingual puzzle.\n"); /* program */
+return (0); /* if successful */
+}
