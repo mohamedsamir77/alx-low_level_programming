4f98f50a038dc6bc7030cff5fc83613aa3c03d27
Mohamed Samir <79877083+mohamedsamir77@users.noreply.github.com>
2023-06-15 17:11:22 +0400

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long int l_int_var;
 long long int ll_int_var;
 float f_var;
 
-printf("Size of a cahr: %li byte(s)\n", sizeof(char_var));
+printf("Size of a char: %li byte(s)\n", sizeof(char_var));
 printf("Size of an int: %li byte(s)\n", sizeof(int_var));
 printf("Size of a long int: %li byte(s)\n", sizeof(l_int_var));
 printf("Size of a long long int: %li byte(s)\n", sizeof(ll_int_var));
