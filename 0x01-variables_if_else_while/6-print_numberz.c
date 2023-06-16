f82ecc351702b798da4affb5aa228ee0a1a05088
Mohamed Samir <79877083+mohamedsamir77@users.noreply.github.com>
2023-06-16 22:01:39 +0400

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  int i;
-  for (i = 48; i < 58; i++)
-    {
-      putchar(i);
-    }
-  putchar("\n");
-  return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
