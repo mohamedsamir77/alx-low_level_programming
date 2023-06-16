448656037077e2abf1b36e81df821dbff32adf68
Mohamed Samir <79877083+mohamedsamir77@users.noreply.github.com>
2023-06-16 22:06:41 +0400

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  int i;
-  for (i = 48; i < 58; i++)
-    {
-      putchar(i);
-      if (i != 57)
-	{
-	  putchar(',');
-	  putchar('');
-	}
-    }
-  putchar("\n");
-  return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar('');
+}
+}
+putchar('\n');
+return (0);
 }
