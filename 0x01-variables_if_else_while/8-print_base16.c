bac70b7e50023c4cd36f2084a1768828834231c1
Mohamed Samir <79877083+mohamedsamir77@users.noreply.github.com>
2023-06-16 22:05:20 +0400

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,17 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  int i;
-  for (i = 48; i < 58; i++)
-    {
-      putchar(i);
-    }
-  fir (i = 97; i < 103; i++)
-    {
-      putchar(i);
-    }
-  putchar("\n");
-  return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
