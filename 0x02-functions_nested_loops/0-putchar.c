07ba02f215b1206ed5aa9ad86024c6f4dd252864
mohamedsamir77 <geo_msamir77@yahoo.com>
2023-06-19 22:29:09 -0700

    task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry program
  *
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-printf("_Putcher");
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+answer the task 0x02-functions_nested_loops
