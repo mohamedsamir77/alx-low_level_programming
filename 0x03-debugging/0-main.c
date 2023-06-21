6804c21ed8a9c6b430c5df335d5439c663e64431
mohamedsamir77 <geo_msamir77@yahoo.com>
2023-06-20 19:33:39 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is zero
+ * @n: integer number to be checked
+ * Return: 0
+*/
+int main(int n)
+{
+positive_or_negative(n);
+if (n == 0)
+printf("%d is zero", n);
+return (0);
+}
