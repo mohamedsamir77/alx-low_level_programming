e74439d6efd953489a8546f3d373cf67224a139a
mohamedsamir77 <geo_msamir77@yahoo.com>
2023-06-22 05:31:22 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - output a diagonal followed by \ and new line
+ * @n: number of times the character \ needed to build the diagonal shape
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n == 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			int j = 0;
+
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}
