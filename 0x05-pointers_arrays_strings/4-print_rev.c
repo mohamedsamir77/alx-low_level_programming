1ba28fa184bbc8c34962bc1bfcd72c54c30488d1
mohamedsamir77 <geo_msamir77@yahoo.com>
2023-06-26 01:53:07 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
